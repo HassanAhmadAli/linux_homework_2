b184931f0889b469e4c90eb4a339a981fabc3d73
hassan.ali <hassan.ali.36900@outlook.com>
2023-05-31 17:13:07 +0300

    fix alive.sh to accept two ipv4 address's

--- code/3/alive.sh
+++ code/3/alive.sh
@@ -1,13 +1,55 @@
-#!/bin/bash
-# alive.sh
-# Checks to see if hosts 192.168.0.1-192.168.0.20 are alive
-# Iterate through IP addressesexp
-for n in {1..20}; do
-    host=192.168.0.$n
-    ping -c2 $host &> /dev/null
+#!/usr/bin/bash
+
+input=$1
+
+# Validate input   
+if [[ $input != *.*.*.*-*.*.*.* ]]; then
+    echo "Invalid input. Please enter in the format of 'ipv4-ipv4'"
+    exit
+fi
+
+# Split IP addresses   
+ip1=$(echo $input | cut -d'-' -f1)  
+ip2=$(echo $input | cut -d'-' -f2)  
+
+# Validate octets 
+for ip in $ip1 $ip2; do
+    oct1=$(echo $ip | cut -d. -f1)
+    oct2=$(echo $ip | cut -d. -f2)
+    oct3=$(echo $ip | cut -d. -f3)
+    oct4=$(echo $ip | cut -d. -f4)
+    for oct in $oct1 $oct2 $oct3 $oct4; do
+        if [[ oct -gt 255 || oct -lt 0 ]]; then 
+            echo "Invalid IP address!"
+            exit 
+        fi
+    done
+
+done
+
+ip_lower="1"
+ip_upper="2"
+# Compare IP addresses
+if [[ $ip1 < $ip2 ]]; then
+    ip_lower=$ip1
+    ip_upper=$ip2
+elif [[ $ip2 < $ip1 ]]; then
+    ip_lower=$ip2
+    ip_upper=$ip1
+else 
+    ip_lower=$ip1
+    ip_upper=$ip2
+fi
+
+ip_lower_num=$(echo $ip_lower | awk -F. '{ print $1*256*256*256 + $2*256*256 + $3*256 + $4 }')
+ip_upper_num=$(echo $ip_upper | awk -F. '{ print $1*256*256*256 + $2*256*256 + $3*256 + $4 }')
+
+for i in $(seq $ip_lower_num $ip_upper_num); do
+    ip=$(echo $i | awk '{printf "%d.%d.%d.%d\n", $1/256/256/256,$1/256/256%256,$1/256%256,$1%256}')
+        ping -c2 $ip &> /dev/null
     if [ $? = 0 ]; then
-    echo "$host is UP"
+    echo "$ip is UP"
     else
-    echo "$host is DOWN"
+    echo "$ip is DOWN"
     fi
 done
