24378f4fa1d8ec9a628c045b43b3d9319fb0e381
hassan.ali <hassan.ali.36900@outlook.com>
2023-05-31 14:46:23 +0300

    comments in first question

--- code/1/1.sh
+++ code/1/1.sh
@@ -6,7 +6,9 @@ if [ "$#" -ne 3 ]; then
   echo "Usage: $0 arg1 arg2 arg3"
   exit 1
 fi
-
+# oldword=$1
+# newword=$2
+# suffix=$3
 for file in `find . -type f -name "*.$3"`; do
     sed -i "s/$1/$2/g" $file
 done
